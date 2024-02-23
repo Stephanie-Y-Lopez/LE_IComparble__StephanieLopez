750acf2ec984e5b2b744d0642bfee07b0684375c
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-02-22 16:28:51 -0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LE_IComparble__StephanieLopez
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LE_IComparble__StephanieLopez
+{
+    internal class Student
+    {
+        //Fields
+        public string _Firstname;
+        public string _Lastname;
+        public double _Csigrade;
+        public double _Genedgrade;
+
+        //Constructor
+        public Student(string Firstname, string Lastname)
+        {
+            _Firstname = Firstname;
+            _Lastname = Lastname;
+            Random Rand = new Random();
+            _Csigrade = Rand.Next(30, 102); //Generates Grade within that number range
+            _Genedgrade = Rand.Next(10, 101); //Generates Grade within that number range
+        }
+
+        //Properties
+        public string FirstName { get => _Firstname; set => _Firstname = value; }
+        public string LastName { get => _Lastname; set => _Lastname = value; }
+        public double CSIGrade { get => _Csigrade; set => _Csigrade = value; }
+        public double GenEdGrade { get => _Genedgrade; set => _Genedgrade = value; }
+
+        public override string ToString()
+        {
+            return $"Name: {_Firstname} {_Lastname} - CSI Grade: {_Csigrade} - Gen Ed Grade: {_Genedgrade}";
+        }
+    }
+}
